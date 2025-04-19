a2cdbaf9b522ad291a350dd141635643d7752785
Jayshwa <josh.young1399@gmail.com>
2025-04-19 22:58:30 +0100

    Initial Commit

---
+++ AccountHolder.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BankingApp{
+    public class AccountHolderClass{
+
+        private string Name { get; set; }
+
+        private float Balance { get; set; }
+
+        public accountholder( string Name, float Balance){
+            Name = Name;
+            Balance = balance
+        }
+
+        public void ShowBalance(){
+            Console.WriteLine($"{Name} has {Balance} GBP")
+
+        }
+
+    }
+}
---
+++ AccountManagement.cs
@@ -0,0 +1,10 @@
+namespace BankingApp
+{
+    public class AccountManagement
+    {
+        public async Task AddFunds(AccountHolder accountHolder, float amount)
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
